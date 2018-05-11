965bede3b840de19dc68f3a8f00944040e2c5199
Remo Giermann <mo@liberejo.de>
2018-05-11 17:20:14 +0200

    adding the current time instead of MTime might be handy too

--- src/Filename.hs
+++ src/Filename.hs
@@ -13,6 +13,7 @@ separationChar = "-"
 -- * Types
 
 type FormatString = String
+data TimestampSource = TimeFileModified | CurrentTime deriving Show
 
 -- * Adding meta information to file paths
 type AddMeta = Adder -> FilePath -> IO FilePath
@@ -20,8 +21,16 @@ type AddMeta = Adder -> FilePath -> IO FilePath
 addString :: String -> AddMeta
 addString s = addInfoToFilePath (getConst s)
 
-addMTime :: FormatString -> TimeLocale -> AddMeta
-addMTime fmt loc = addInfoToFilePath (getTimestamp fmt loc)
+addTimestamp :: TimestampSource -> FormatString -> TimeLocale -> AddMeta
+addTimestamp src fmt loc = addInfoToFilePath $ uncurry (get src) (fmt, loc)
+    where get TimeFileModified = getModTimestamp 
+          get CurrentTime = getCurrentTimestamp
+
+addModTime :: FormatString -> TimeLocale -> AddMeta
+addModTime = addTimestamp TimeFileModified
+
+addCurTime :: FormatString -> TimeLocale -> AddMeta
+addCurTime = addTimestamp CurrentTime 
 
 addInfoToFilePath :: Getter -> AddMeta
 addInfoToFilePath get add fp = add fp <$> get fp
@@ -32,8 +41,11 @@ type Getter = FilePath -> IO String
 getConst :: String -> FilePath -> IO String
 getConst s _ = return s
 
-getTimestamp :: FormatString -> TimeLocale -> FilePath -> IO String
-getTimestamp fmt loc fp = formatTime loc fmt <$> getModificationTime fp
+getModTimestamp :: FormatString -> TimeLocale -> FilePath -> IO String
+getModTimestamp fmt loc fp = formatTime loc fmt <$> getModificationTime fp
+
+getCurrentTimestamp :: FormatString -> TimeLocale -> FilePath -> IO String
+getCurrentTimestamp fmt loc _ = formatTime loc fmt <$> getCurrentTime 
 
 -- * Adders: adding strings to file paths 
 type Adder = FilePath -> String -> FilePath
