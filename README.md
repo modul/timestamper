cf81b5bb75355b32adc51c84fd1b47ee73b6c9f8
Remo Giermann <mo@liberejo.de>
2019-08-31 20:14:42 +0200

    updated documentation

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# timestamper
-Adding timestamps to files or directories.
+# Timestamper
+
+timestamper is a commandline utility to add timestamps to the names of files or directories.
+
+## Usage
+
+```sh
+timestamper [OPTIONS] [FILE]
+
+Common flags:
+  -d    --dry              don't do anything, just print actions
+  -m    --modified         use the modification time as a timestamp (default)
+  -n    --now              use the current time as a timestamp
+  -f    --format=ITEM      timestamp format (default: %y%m%d-%H%M)
+  -b    --before           put timestamp before the filename (default)
+  -a    --after            put timestamp after the filename
+        --textbefore=ITEM  optional text before filename
+        --textafter=ITEM   optional text after filename
+  -h -? --help             Display help message
+  -V    --version          Print version information
+        --numeric-version  Print just the version number
+  -v    --verbose          Loud verbosity
+  -q    --quiet            Quiet verbosity
+
+```
+
+## Examples
+
+```sh
+
+timestamper -v -n basic.txt
+basic.txt -> 180513-1753-basic.txt
+
+timestamper -v -a --textbefore Test package.yaml
+package.yaml -> Test-package-180510-1512.yaml
+```
--- src/Options.hs
+++ src/Options.hs
@@ -19,20 +19,20 @@ data Options = Options {
                } deriving (Show, Data, Typeable)
 
 options = Options {
-            dry = False &= help "don't do anything, just print actions",
+            dry = False &= help "Don't do anything, just print actions",
             source = enum [
-                TimeFileModified &= help "use the modification time as a timestamp (default)" &= name "modified" &= name "m" &= explicit ,
-                TimeNow &= help "use the current time as a timestamp" &= name "now" &= name "n" &= explicit],
-            format = defaultFormat &= help ("timestamp format (default: " ++ defaultFormat ++ ")"),
+                TimeFileModified &= help "Use the modification time as a timestamp (default)" &= name "modified" &= name "m" &= explicit ,
+                TimeNow &= help "Use the current time as a timestamp" &= name "now" &= name "n" &= explicit],
+            format = defaultFormat &= help ("Timestamp format (default: " ++ defaultFormat ++ ")"),
             placement = enum [
-                Before &= help "put timestamp before the filename (default)", 
-                After &= help "put timestamp after the filename"],
-            textBefore = "" &= help "optional text before filename", 
-            textAfter = "" &= help "optional text after filename",
+                Before &= help "Put timestamp before the filename (default)", 
+                After &= help "Put timestamp after the filename"],
+            textBefore = "" &= help "Optional text before filename", 
+            textAfter = "" &= help "Optional text after filename",
             files = def &= typFile &= args 
           } &= program "timestamper"
             &= helpArg [name "h"]
-            &= help "Add a timestamp (file modified or current time) and optional text to one or more file paths. Works on files and directories."
+            &= help "Adds a timestamp and optional text to one or more file or directory names."
             &= summary ("timestamper v" ++ showVersion version)
             &= details ["Examples:",
                         "  timestamper -v -n basic.txt",
